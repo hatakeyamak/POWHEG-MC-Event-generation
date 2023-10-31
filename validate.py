0975de6c5432965dcd2f9393233a1e985e3dd387
Jan van der Linden <jan.vdlinden@cern.ch>
2023-10-31 15:13:01 +0100

    validate

--- validate.py
+++ validate.py
@@ -2,25 +2,34 @@ import os
 import sys
 import glob
 
-def get_expected_files(stage):
-    if stage==1:
+
+def get_expected_files(stage, iteration=1):
+    if stage==1 and iteration==1:
         expected_files = [
-            "pwg-{jobid:04d}-xg{it}-stat.dat",
-            "pwgcounters-st{it}-{jobid:04d}.dat",
-            "pwg-xg{it}-xgrid-btl-{jobid:04d}.dat",
-            "pwg-xg{it}-xgrid-rm-{jobid:04d}.dat"
+            "pwg-{jobid:04d}-xg1-stat.dat",
+            "pwgcounters-st1-{jobid:04d}.dat",
+            "pwg-xg1-xgrid-btl-{jobid:04d}.dat",
+            "pwg-xg1-xgrid-rm-{jobid:04d}.dat"
+            ]
+    elif stage==1 and iteration>1:
+        expected_files = [
+            f"pwg-{{jobid:04d}}-xg{iteration}-stat.dat",
+            f"pwg-xg{iteration}-xgrid-btl-{{jobid:04d}}.dat",
+            f"pwg-xg{iteration}-xgrid-rm-{{jobid:04d}}.dat",
+            f"pwg-xg{iteration}-xgrid-btl-{{jobid:04d}}.top",
+            f"pwg-xg{iteration}-xgrid-rm-{{jobid:04d}}.top"
             ]
     elif stage==4:
         expected_files = [] # TODO
     return expected_files
     
 def check_stage_output(settings, nbatches, stage, iteration, workdir, any_exist=False):
-    expected_files = get_expected_files(stage)
+    expected_files = get_expected_files(stage, iteration)
     
     missing_ids = []
     for n in range(nbatches):
         for f in expected_files:
-            f_formatted = f.format(jobid=n, it=iteration)
+            f_formatted = f.format(jobid=n)
             f_dir = os.path.join(settings['run_dir'], f_formatted)
             if os.path.exists(f_dir) and any_exist:
                 # any_exist --> return true as soon as one file exists
